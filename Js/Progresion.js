4c5baa2f1f0d7d13c2460d1ba79b23897e70cf5d
joshtin2505 <justincastro2505@gmail.com>
2023-04-14 17:54:37 -0500

    Progresion act 2

--- Js/Progresion.js
+++ Js/Progresion.js
@@ -1,106 +1,179 @@
-function progRandom() {
-    let notes = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B'];
-    let types = ['', 'm', 'sus2', 'sus4', 'sus6', 'sus9', '6', '7', 'Maj7', 'm7', 'dim', 'aug', 'add6', 'add9', 'add11', '6', '9', '11', '13'];
-    let escalas = ['Major', 'Minor', 'Pentatonic', 'Pentatonic Minor', 'Blues'];
+const c = ['C', 'Cm', 'Csus2', 'Csus4', 'Csus6', 'Csus9', 'C6', 'C7', 'CMaj7', 'Cm7', 'Cdim', 'Caug', 'Cadd6', 'Cadd9', 'Cadd11', 'C6', 'C9', 'C11', 'C13']
+const cSharp = ['C#', 'C#m', 'C#sus2', 'C#sus4', 'C#sus6', 'C#sus9', 'C#6', 'C#7', 'C#Maj7', 'C#m7', 'C#dim', 'C#aug', 'C#add6', 'C#add9', 'C#add11', 'C#6', 'C#9', 'C#11', 'C#13']
+const d = ['D', 'Dm', 'Dsus2', 'Dsus4', 'Dsus6', 'Dsus9', 'D6', 'D7', 'DMaj7', 'Dm7', 'Ddim', 'Daug', 'Dadd6', 'Dadd9', 'Dadd11', 'D6', 'D9', 'D11', 'D13'];
+const eb = ['Eb', 'Ebm', 'Ebsus2', 'Ebsus4', 'Ebsus6', 'Ebsus9', 'Eb6', 'Eb7', 'EbMaj7', 'Ebm7', 'Ebdim', 'Ebaug', 'Ebadd6', 'Ebadd9', 'Ebadd11', 'Eb6', 'Eb9', 'Eb11', 'E13'];
+const e = ['E', 'Em', 'Esus2', 'Esus4', 'Esus6', 'Esus9', 'E6', 'E7', 'EMaj7', 'Em7', 'Edim', 'Eaug', 'Eadd6', 'Eadd9', 'Eadd11', 'E6', 'E9', 'E11', 'E13'];
+const f = ['F', 'Fm', 'Fsus2', 'Fsus4', 'Fsus6', 'Fsus9', 'F6', 'F7', 'FMaj7', 'Fm7', 'Fdim', 'Faug', 'Fadd6', 'Fadd9', 'Fadd11', 'F6', 'F9', 'F11', 'F13'];
+const fSharp = ['F#', 'F#m', 'F#sus2', 'F#sus4', 'F#sus6', 'F#sus9', 'F#6', 'F#7', 'F#Maj7', 'F#m7', 'F#dim', 'F#aug', 'F#add6', 'F#add9', 'F#add11', 'F#6', 'F#9', 'F#11', 'F#13'];
+const g = ['G', 'Gm', 'Gsus2', 'Gsus4', 'Gsus6', 'Gsus9', 'G6', 'G7', 'GMaj7', 'Gm7', 'Gdim', 'Gaug', 'Gadd6', 'Gadd9', 'Gadd11', 'G6', 'G9', 'G11', 'G13'];
+const gSharp = ['G#', 'G#m', 'G#sus2', 'G#sus4', 'G#sus6', 'G#sus9', 'G#6', 'G#7', 'G#Maj7', 'G#m7', 'G#dim', 'G#aug', 'G#add6', 'G#add9', 'G#add11', 'G#6', 'G#9', 'G#11', 'G#13'];
+const a = ['A', 'Am', 'Asus2', 'Asus4', 'Asus6', 'Asus9', 'A6', 'A7', 'AMaj7', 'Am7', 'Adim', 'Aaug', 'Aadd6', 'Aadd9', 'Aadd11', 'A6', 'A9', 'A11', 'A13'];
+const bb = ['Bb', 'Bbm', 'Bbsus2', 'Bbsus4', 'Bbsus6', 'Bbsus9', 'Bb6', 'Bb7', 'BbMaj7', 'Bbm7', 'Bbdim', 'Bbaug', 'Bbadd6', 'Bbadd9', 'Bbadd11', 'Bb6', 'Bb9', 'Bb11', 'Bb13'];
+const b = ['B', 'Bm', 'Bsus2', 'Bsus4', 'Bsus6', 'Bsus9', 'B6', 'B7', 'BMaj7', 'Bm7', 'Bdim', 'Baug', 'Badd6', 'Badd9', 'Badd11', 'B6', 'B9', 'B11', 'B13'];
+function escalaMayor(nacordes,tono){
+    const structreC = [c,d,e,f,g,a,b];
+    const structreCsh = [cSharp,eb,f,fSharp,gSharp,bb,b];
+    const structreD = [d,e,fSharp,g,a,b,cSharp];
+    const structreEb = [e,fSharp,gSharp,a,b,cSharp,eb];
+    const structreF = [f,g,a,bb,c,d,e];
+    const structureFsh = [fSharp,gSharp,bb,b,cSharp,eb,f];
+    const structreG = [g,a,b,c,d,e,fSharp];
+    const structreGsh = [gSharp,bb,c,cSharp,eb,f,g];
+    const structreA = [a,b,cSharp,d,e,fSharp,gSharp];
+    const structreBb = [bb,c,d,eb,f,g,a];
+    const structreB = [b,cSharp,eb,e,fSharp,gSharp,bb];
+    let concat = "";
+    var prog = "";
+    let note = 0;
+    let chord = 0;
 
+    if(tono == 1){
+        for(let i = 0 ; i < nacordes; i++){
+            note = Math.floor(Math.random() * structreA.length)
+            chord = Math.floor(Math.random() * c.length)
+            prog = structreC[note][chord];
+            concat = concat + ' | '+ prog;
+        }
+        return concat;
+        console.log(concat)
+    }
+    else if (tono == 2){
+        for(let i = 0 ; i < nacordes; i++){
+            note = Math.floor(Math.random() * structreA.length)
+            chord = Math.floor(Math.random() * c.length)
+            prog = structreCsh[note][chord];
+            concat = concat + ' | '+ prog;
+        }
+        return concat;
+        
+    }
+}
+function progRandom() {
+        
     let texto_respuesta = document.getElementById('texto-respuesta');
     let miSelect_nC = document.getElementById('miSelect');
     let miSelect2_T = document.getElementById('miSelect2');
-    let miSelect3_M = document.getElementById('miSelect3')
+    let miSelect3_M = document.getElementById('miSelect3');
 
-    let selectC = miSelect_nC.value;
-    let selectT = miSelect2_T.value;
-    let selectM = miSelect3_M.value
-    //nchords - genere - modo
+    let selectC = Number(miSelect_nC.value);
+    let selectT = Number(miSelect2_T.value);
+    let selectM = Number(miSelect3_M.value);
 
-    let randomT;
-    switch (selectT) {
-        case 0:
-            randomT = Math.floor(Math.random() * notes.length);
-
-            break;
-        case 1:
-            randomT = 0;
-            break;
-        case 2:
-            randomT = 1;
-            break;
-        case 3:
-            randomT = 2;
-            break;
-        case 4:
-            randomT = 3;
-            break;
-        case 5:
-            randomT = 4;
-            break;
-        case 6:
-            randomT = 5;
-            break;
-        case 7:
-            randomT = 6;
-            break;
-        case 8:
-            randomT = 7;
-            break;
-        case 9:
-            randomT = 8;
-            break;
-        case 10:
-            randomT = 9;
-            break;
-        case 11:
-            randomT = 10;
-            break;
-        case 12:
-            randomT = 11;
-            break;
-        default:
-            break;
+    //En casos de que el usuario nos escoja nada
+    let nAcordesEscojidos
+    if(selectC == 0){
+        nAcordesEscojidos = Math.floor(Math.random() * 10);
     }
-    let randomM;
+    else if (selectC == 1){
+        nAcordesEscojidos = 1;
+    }
+    else if (selectC == 2){
+        nAcordesEscojidos = 2;
+    }
+    else if (selectC == 3){
+        nAcordesEscojidos = 3;
+    }
+    else if (selectC == 4){
+        nAcordesEscojidos = 4;
+    }
+    else if (selectC == 5){
+        nAcordesEscojidos = 5;
+    }
+    else if (selectC == 6){
+        nAcordesEscojidos = 6;
+    }
+    else if (selectC == 7){
+        nAcordesEscojidos = 7;
+    }
+    else if (selectC == 8){
+        nAcordesEscojidos = 8;
+    }
+    else if (selectC == 9){
+        nAcordesEscojidos = 9;
+    }
+    else if (selectC == 10){
+        nAcordesEscojidos = 10;
+    }
+
+    
+    let tonoEscojido;
+    if (selectT > -1){
+        if(selectT == 0){
+            tonoEscojido = Math.floor(Math.random() * 10);
+        }
+        else if (selectT == 1){
+            tonoEscojido = 1;
+        }
+        else if (selectT == 2){
+            tonoEscojido = 2;
+        }
+        else if (selectT == 3){
+            tonoEscojido = 3;
+        }
+        else if (selectT == 4){
+            tonoEscojido = 4;
+        }
+        else if (selectT == 5){
+            tonoEscojido = 5;
+        }
+        else if (selectT == 6){
+            tonoEscojido = 6;
+        }
+        else if (selectT == 7){
+            tonoEscojido = 7;
+        }
+        else if (selectT == 8){
+            tonoEscojido = 8;
+        }
+        else if (selectT == 9){
+            tonoEscojido = 9;
+        }
+        else if (selectT == 10){
+            tonoEscojido = 10;
+        }
+        else if (selectT == 11){
+            tonoEscojido = 11;
+        }
+        else if (selectT == 0){
+            tonoEscojido = 0;
+        }}
+
+    let modoEscojido;
     switch (selectM) {
         case 0:
-            randomM = Math.floor(Math.random() * notes.length);
+            modoEscojido = Math.floor(Math.random() * 6);
             break;
         case 1:
-            randomM = 0;
+            modoEscojido = 0;
             break;
         case 2:
-            randomM = 1;
+            modoEscojido = 1;
             break;
         case 3:
-            randomM = 2;
+            modoEscojido = 2;
+            break;
         case 4:
-            randomM = 3;
+            modoEscojido = 3;
             break;
         case 5:
-            randomM = 4;
+            modoEscojido = 4;
             break;
         case 6:
-            randomM = 5;
+            modoEscojido = 5;
             break;
         default:
             break;
     }
-
-    let tandomC;
-    let escojeTone;
-    let escojeChord;
-    let escojeMode;
-    ///Tengo que hacer que la maquina entienda que scala se tomó y que modo es el que esta para escojer un un acorde o progrecion que este dentro del rango, puedo hacerlo creando una lista de intervalos 
-    if (selectC == 0) {
-        //por si el selector es 0, para escojer la cantidad de acordes que eseran aleatoreamente 
-        tandomC = Math.floor(Math.random() * 10) + 1;
-    }
-    else if (selectC == 1) {
-        for(let i = 0; i < selectC; i++) {
-            escojeTone = Math.floor(Math.random () * notes.length);
-            escojeMode = Math.floor(Math.random() * escalas.length) ;
-        }
-        texto_respuesta.innerHTML = `Tu Acorde Es: ${notes[escojeChord]}`;
-    }
+    console.log("m"+modoEscojido)
+    console.log("t"+tonoEscojido)
+    console.log("n"+nAcordesEscojidos)
     
+//Primero se toma el tono escojido luego se itera sobre los acordes escojidos en el modo escojido
+    if(modoEscojido == 0) {
+        let retunet = escalaMayor(nAcordesEscojidos,tonoEscojido);
+        let nreturnet = retunet.slice(2);
+        texto_respuesta.innerHTML = nreturnet.trim(); 
 
-    
+    }
+   
 }
